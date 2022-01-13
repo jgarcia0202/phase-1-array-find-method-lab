691a36a1acc26cef0f0c22747d83f9e7decfc96f
Jose <jmgarcia0202@gmail.com>
2022-01-12 16:55:25 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+// code your solution here
+let superbowlWin = (array) => {
+    let result = undefined;
+    array.find((play) => {
+        if (play.result === "W") {
+        console.log (play.year)
+        result = play.year;
+    }
+});
+    return result;
+}
